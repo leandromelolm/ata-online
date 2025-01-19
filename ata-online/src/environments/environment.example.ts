3bdf76d66513e3d2a376452d92c2b5f8a22026b5
leandromelolm <leandrosiqueira.pe@gmail.com>
2025-01-19 13:52:09 -0300

    doc(environment): exemplo

--- ata-online/src/environments/environment.example.ts
+++ ata-online/src/environments/environment.example.ts
@@ -2,4 +2,10 @@ export const environment = {
   production: false,
   scriptId:'',
   cryptoSecret: '',
-};  
+  userTest: '',
+  passwordUserTest: '',
+};
+
+// criar os arquivos
+// environment.prod.ts
+// environment.ts
