41090fd2f12b5fa2be49c7bb68a6fa13fd306e69
lsm <lsm@a.recife.ifpe.edu.br>
2026-09-05 01:08:24 +0000

    build

--- ata-online/src/app/pages/index/index.component.ts
+++ ata-online/src/app/pages/index/index.component.ts
@@ -10,11 +10,14 @@ import { environment } from '../../../environments/environment';
 export class IndexComponent {
 
   titleMain: string = "AtaOnline";
+  background_title: string = "ata-online";
+
 
   constructor(private router: Router) {}
 
   ngOnInit() {
     this.titleMain = environment.titleMain;
+    this.background_title = environment.title
     sessionStorage.clear();
 
   }
